5c86411bea38c57cf8e7bc62458fadc3f7e5f093
Siarhei Puhach <siarhei.puhach@itechart-group.com>
2017-08-11 10:07:18 +0300

    Added flexible styles to events

--- src/EventInstance/EventInstance.jsx
+++ src/EventInstance/EventInstance.jsx
@@ -16,29 +16,40 @@ export default class EventInstance extends Component {
     }
 
     render() {
-        const { event, intersections, eventsCount, instance, day } = this.props;
+        const { event, intersections, day, daysInBoard } = this.props;
         const momentStartDate = moment(event['startDate']);
         const momentEndDate = moment(event['endDate']);
 
         const duration = momentEndDate.diff(momentStartDate, 'minutes');
         const decimalHours = parseFloat(momentStartDate.get('hour')) + parseFloat(momentStartDate.get('minute') / 60);
 
-        // TODO Get rid of magic constants and do style is flexible
         const dayAmount = day ? day : 0;
-        const fourDaysLeft = this.props.daysInBoard === 4 ? 150 : 0;
+        let leftPositionOneDay = intersections > 0 ?
+            String(7 + intersections * 2) +'%'
+            :
+            ('7%');
+        let leftPositionWeek = intersections > 0 ?
+            String(64 + dayAmount*116 + intersections * 2) +'%'
+            :
+            String(64 + dayAmount*116) + '%';
+
+        let leftPositionFourDays = intersections > 0 ?
+            String(36 + dayAmount*116 + intersections * 2) +'%'
+            :
+            String(36 + dayAmount*116) + '%';
+
+        let leftPosition = daysInBoard === 1 ?
+            leftPositionOneDay
+            :
+            daysInBoard === 7 ? leftPositionWeek : leftPositionFourDays;
+
         return (
             <div className="eventContainer"
                  style={{
                      backgroundColor: this.getRandomColor(),
                      height: duration,
                      top: 60 + decimalHours * 60,
-                     left: intersections > 0
-                         ?
-                         (105+dayAmount*193 + fourDaysLeft*(dayAmount) ) + intersections * 20 + (
-                             (1500 / eventsCount)-(105 + intersections * 20)
-                         ) * (instance - 1)
-                         :
-                         (105+dayAmount*193 + fourDaysLeft*(dayAmount)),
+                     left: leftPosition,
                      zIndex: intersections,
                      opacity: 0.75
                  }}>
